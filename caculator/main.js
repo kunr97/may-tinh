d8fc3ed4a45747b6aafd132d41b6411fcba8aff5
kunr97 <58990453+kunr97@users.noreply.github.com>
2022-11-22 23:06:18 +0700

    Add files via upload

---
+++ caculator/main.js
@@ -0,0 +1,47 @@
+var allNnumberButton = document.querySelectorAll(".number_button");
+var renderDisplay = document.getElementById("display");
+var deleteButton = document.querySelector(".delete_button");
+var resultButton = document.querySelector(".result_button");
+var plusButton = document.querySelector(".plus_button");
+var minusButton = document.querySelector(".minus_button");
+var divisionButton = document.querySelector(".division_button");
+var multiplierButton = document.querySelector(".multiplier_button");
+var memoryTemp;
+function print(value,index){
+    value.onclick = function() {
+        if(renderDisplay.innerText.length < 15){
+            renderDisplay.innerText = renderDisplay.innerText.concat(value.innerText);
+        }
+    }
+}
+
+allNnumberButton.forEach(print);
+
+deleteButton.onclick = function(){
+    lengthRender = renderDisplay.innerText.length;
+    renderDisplay.innerText = renderDisplay.innerText.slice(0,lengthRender-1);
+}
+
+plusButton.onclick = function(){
+    renderDisplay.innerText = renderDisplay.innerText.concat("+");
+}
+
+resultButton.onclick = function(){
+    renderDisplay.innerText =  eval(renderDisplay.innerText);
+}
+
+minusButton.onclick = function(){
+    renderDisplay.innerText = renderDisplay.innerText.concat("-");
+}
+
+divisionButton.onclick = function(){
+    renderDisplay.innerText = `(${renderDisplay.innerText}) / `;
+    
+}
+
+multiplierButton.onclick = function(){
+    renderDisplay.innerText = `(${renderDisplay.innerText}) * `;
+
+}
+
+
